d6019daf9043625c8398cc03a493a1e01cf75823
go-laoji <us.hipeace86@gmail.com>
2022-02-10 14:32:09 +0800

    三方登陆

--- src/pages/3rd/Scan/index.tsx
+++ src/pages/3rd/Scan/index.tsx
@@ -1,32 +1,33 @@
-import {PageContainer} from '@ant-design/pro-layout';
-import {useEffect} from 'react';
-import {Row, Col} from 'antd';
+import { PageContainer } from '@ant-design/pro-layout';
+import { useEffect } from 'react';
+import { Row, Col, List } from 'antd';
 import styles from './index.less';
 
 export default () => {
+  const oauthUrl = encodeURI(`http://${process.env.OauthHost}/login/oauth`);
+  const loginUrl = `https://open.work.weixin.qq.com/wwopen/sso/3rd_qrConnect?appid=${process.env.AppId}&redirect_uri=${oauthUrl}&state=STATE&usertype=admin`;
+  const notices = [
+    '服务商后台/应用管理/登录授权发起域名（跳转后会判断referer）',
+    '服务商后台/应用管理/授权完成回调域名（扫码后跳转的域名）',
+  ];
   useEffect(() => {
-    var wwLogin = new window.WwLogin({
-      "id": "qrcode",
-      "appid": process.env.AppId,
-      "agentid": process.env.AgentId,
-      "redirect_uri": encodeURI(`http://${process.env.OauthHost}/login/oauth`),
-      "state": "",
-      "lang": "zh",
-    });
+    setTimeout(() => {
+      window.location.href = loginUrl;
+    }, 3000);
   }, []);
   return (
-    <PageContainer className={styles.main}>
-      <div style={{paddingTop: 100, textAlign: 'center'}}>
+    <PageContainer content="3秒后自动跳转" className={styles.main}>
+      <div style={{ paddingTop: 100, textAlign: 'center' }}>
         <Row>
-          <Col xs={{span: 5, offset: 1}} lg={{span: 6, offset: 2}}>
-
+          <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}></Col>
+          <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            <List
+              header={<div>注意配置项</div>}
+              dataSource={notices}
+              renderItem={(item) => <List.Item>{item}</List.Item>}
+            />
           </Col>
-          <Col xs={{span: 11, offset: 1}} lg={{span: 6, offset: 2}}>
-            <div id='qrcode'></div>
-          </Col>
-          <Col xs={{span: 5, offset: 1}} lg={{span: 6, offset: 2}}>
-
-          </Col>
+          <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}></Col>
         </Row>
       </div>
     </PageContainer>
--- src/pages/oauth.tsx
+++ src/pages/oauth.tsx
@@ -1,18 +1,24 @@
 import { useState, useEffect } from 'react';
+import { useSearchParam } from 'react-use';
 import { Spin } from 'antd';
 
 export default function Oauth() {
   const [loading, setLoading] = useState<boolean>(true);
+  let code = useSearchParam('code');
+  if (code === undefined || code == '') {
+    code = useSearchParam('auth_code');
+  }
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 3000);
+    // TODO: 拿code去后台接口登陆换取登陆信息
   }, []);
   return (
-    <div className='ant-pro-page-container'>
+    <div className="ant-pro-page-container">
       <div style={{ paddingTop: 100, textAlign: 'center' }}>
         <Spin spinning={loading} size="large" />
-        <div>正在登陆中....</div>
+        <div>正在登陆中....{code}</div>
       </div>
     </div>
   );
